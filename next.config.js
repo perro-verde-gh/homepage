8b35b843b144341a3d23f86a6fb33cda179538c6
Alberto Torres <albertotorresfoltyn@users.noreply.github.com>
2026-02-15 22:48:31 -0300

    Refactor basePath logic for Cloudflare and GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,19 +1,23 @@
 /** @type {import('next').NextConfig} */
+const isCloudflare = process.env.CF_PAGES === '1'
 const isGithubActions = process.env.GITHUB_ACTIONS === 'true'
 const repository = process.env.GITHUB_REPOSITORY || ''
 const repositoryName = repository.split('/')[1] || ''
 const isUserOrOrgPages = repositoryName.endsWith('.github.io')
-const basePath =
-  isGithubActions && repositoryName && !isUserOrOrgPages ? `/${repositoryName}` : ''
+
+// No basePath para Cloudflare, solo para GitHub Pages
+const basePath = isCloudflare 
+  ? '' 
+    : (isGithubActions && repositoryName && !isUserOrOrgPages ? `/${repositoryName}` : '')
 
 const nextConfig = {
-  output: 'export',
-  trailingSlash: true,
-  basePath,
-  assetPrefix: basePath,
-  images: {
-    unoptimized: true,
-  },
+    output: 'export',
+    trailingSlash: true,
+    basePath,
+    assetPrefix: basePath,
+    images: {
+          unoptimized: true,
+    },
 }
 
 module.exports = nextConfig
